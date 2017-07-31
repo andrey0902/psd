a0058d8c9b748f875fab39bc61e8389dbfab58ee
Andrei <andreypavlov1706@gmail.com>
2017-07-31 23:45:16 +0300

    full

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+let gulp = require('gulp'),
+    ts = require('gulp-typescript'),
+    tsProject = ts.createProject('tsconfig.json'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs'),
+    browserify = require("browserify"),
+    source = require('vinyl-source-stream'),
+    tsify = require("tsify"),
+    babelify = require('babelify');
+
+// Создание таска common.js
+gulp.task('scripts-js', function () {
+    return browserify({
+        entries: ['app/js/app.js', 'app/js/index.js'],
+        extensions: ['.js'],
+        debug: true
+    })
+        .transform('babelify', {
+            presets: ["es2015", "react"],
+            plugins: ['transform-class-properties']
+        })
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('app/production'))
+        .pipe(browserSync.reload({ stream: true }));
+});
+
+
+
+// Создание таска стилей
+gulp.task('sass', function () { // Создаем таск "sass"
+    return gulp.src('sass/**/*.+(scss|sass)') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('./css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+// Создание таска browser-sync для автомат. обновления
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: './'
+        },
+        notify: false
+    });
+});
+
+// Создание watcher
+gulp.task('watch', ['browser-sync', 'sass', /*'scripts-js'*/], function () {
+    gulp.watch('./sass/**/*.+(scss|sass)', ['sass']);
+    gulp.watch('./**/*.html', browserSync.reload);
+    gulp.watch('./scripts/**/*.js', browserSync.reload);
+    gulp.watch('./js/**/*.+(jsx|js)', ['scripts-js']);
+    // gulp.watch('app/ts/**/*.ts', ['scripts-ts']);
+});
+
+gulp.task('default', ['watch']);
+
+
+
+
+
+
+
+/*
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/!**!/!*.scss', ['sass']);
+});
+
+gulp.task('minCss',function () {
+    return gulp.src('css/!**!/!*.css')
+        .pipe(minifyCss({KeepBreak:true}))
+        .pipe(gulp.dest('app/css/'))
+});
+
+gulp.task('minJson', function () {
+    gulp.src('./!**!/!*.json')
+        .pipe(jsonmin())
+        .pipe(gulp.dest('./app/'));
+});*/
---
+++ main.js
@@ -0,0 +1,151 @@
+(()=>{
+    let iconSearch = document.querySelector('.my-search');
+    let blockShow = document.querySelector('.drop-search');
+    iconSearch.addEventListener('click', ()=> {
+
+
+        if(blockShow.style.display === 'flex'){
+            blockShow.style.display = 'none';
+        }else{
+            blockShow.style.display = 'flex';
+        }
+    });
+
+    let li = document.querySelectorAll('.item');
+    let pause = document.querySelector('.pause');
+    let next = document.querySelector('.next');
+    let previous = document.querySelector('.previous');
+    let active = true;
+    let count = 0;
+    let idTimer = null;
+    let stop = () => {
+        pause.innerText = 'play';
+        clearInterval(idTimer);
+        active = false;
+    };
+    previous.addEventListener('click', ()=> {
+        stop();
+        toggle(count -1)
+    })
+    next.addEventListener('click', ()=> {
+        stop();
+        toggle(count + 1)
+    })
+    function toggle(n) {
+        li[count].className = 'item';
+        count = (n + li.length)% li.length;
+        li[count].className = 'item active';
+    }
+
+
+    let play = () => {
+        pause.innerText = 'pause'
+        active = true;
+        idTimer = setInterval(()=> {
+            appears()
+        }, 2000)
+    }
+    pause.addEventListener('click', ()=> {
+        if(active) {
+            stop();
+            return;
+        }
+        play();
+    });
+    function appears() {
+        li[count].classList = 'item';
+        count = (count+1)% li.length;
+        li[count].classList = 'item active';
+    }
+    idTimer = setInterval(()=> {
+        appears()
+    }, 2000);
+
+    $('.owl-carousel').owlCarousel({
+        autoWidth: true,
+        loop: true,
+    /*    center: true,*/
+        margin: 0,
+        responsive:{
+            320:{
+                items: 1,
+            },
+            420:{
+                items: 2,
+            },
+            1200:{
+                items:6,
+            }
+        }
+    });
+    $( "#accordion" ).accordion({
+        collapsible: true,
+        icons: { "header": "ui-icon-plusthick", "activeHeader": "ui-icon-minus" },
+        heightStyle: 'content'
+    });
+    let toggleMenu = $('.toggle-menu');
+        toggleMenu.on('click', () => {
+            toggleMenu.find('i').toggleClass('fa-bars');
+            toggleMenu.find('i').toggleClass('fa-chevron-down');
+            $('.menu-1').toggleClass('display');
+    });
+/*
+    let substrate = document.querySelector('.substrate');
+    let buttonClose = document.querySelector('.content-pop-up__exit');
+    let buttonExit = document.querySelector('.content-pop-up__close');
+    let popUpBody = document.querySelector('.content-pop-up__body');
+    let buttonArray = document.querySelectorAll('.project-title__my-search');
+    let createImg =(element) => {
+        return element.cloneNode(false);
+    };
+    let addClass = (element) => {
+        return element.classList = 'content-pop-up__img';
+    };
+    let appendImg = (img)=> {
+        popUpBody.appendChild(img) ;
+    }
+    for(let button of buttonArray) {
+        button.addEventListener('click', (e)=> {
+           let imgOld =  document.querySelector('.content-pop-up__img');
+           if(imgOld)
+               imgOld.remove();
+           if(e.target.parentNode.previousElementSibling.classList.value ==='project-title__img'){
+             let elem =  createImg(e.target.parentNode.previousElementSibling);
+             let img = addClass(elem);
+             appendImg(elem);
+           }else{
+               let elem =  createImg(e.target.parentNode.parentNode.previousElementSibling);
+               let img = addClass(elem);
+               appendImg(elem);
+           }
+
+            substrate.style.display = 'block';
+        });
+    }
+
+    let close = () => {
+        substrate.style.display = 'none';
+    };
+    buttonClose.addEventListener('click', close);
+    buttonExit.addEventListener('click', close);
+    document.body.addEventListener('click', (e)=> {
+         if (e.target.classList.value === 'substrate')
+             close();
+    })*/
+/*    let firstImg = $('.toggle-img__first');
+    let lastImg = $('.toggle-img__last');
+    firstImg.on('click', function (e) {
+        $(this).toggleClass('img-active');
+        lastImg.toggleClass('img-active');
+    });
+    lastImg.on('click', function (e) {
+        $(this).toggleClass('img-active');
+        firstImg.toggleClass('img-active');
+    });*/
+    $('.owl-carousel').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery:{enabled:true}
+        // other options
+    });
+})();
